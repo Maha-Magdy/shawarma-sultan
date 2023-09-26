68c4ee40c4da1030a46d8aa3d0b13c464d924bd1
Maha Magdy <mahamagdy3@gmail.com>
2023-09-26 08:56:30 +0300

    Finalize the hero section, and work on the responsive for this section

--- app/header.tsx
+++ app/header.tsx
@@ -5,16 +5,16 @@ interface Props {
 }
 
 const Header = ({ showSideMenuHandler }: Props) => (
-    <header className='py-2 md:py-4 px-4 md:px-8 sticky top-0 bg-white	border-b border-solid border-[#3434341a]'>
-        <div className='md:container md:mx-auto'>
-            <div className="grid grid-cols-3 items-center block md:hidden">
+    <header className='py-2 lg:py-4 px-4 lg:px-8 sticky top-0 bg-white	border-b border-solid border-[#3434341a]'>
+        <div className='lg:container lg:mx-auto'>
+            <div className="grid grid-cols-3 items-center block lg:hidden">
                 <button
                     className='w-fit'
                     onClick={showSideMenuHandler}
                 >
                     <span className="material-symbols-rounded text-4xl">menu</span>
                 </button>
-                <div className='justify-self-center w-16 md:w-32 h-16 md:h-32 relative'>
+                <div className='justify-self-center w-16 lg:w-32 h-16 lg:h-32 relative'>
                     <Image
                         src="/assets/images/shawarma-sultan-logo-with-text.png"
                         alt="Shawarma Sultan Logo"
@@ -28,7 +28,7 @@ const Header = ({ showSideMenuHandler }: Props) => (
                 </div>
             </div>
 
-            <div className='hidden md:inline-flex justify-between items-center w-full'>
+            <div className='hidden lg:inline-flex justify-between items-center w-full'>
                 <div className='flex items-center gap-1'>
                     <div className='justify-self-center w-14 h-14 relative'>
                         <Image
@@ -38,7 +38,7 @@ const Header = ({ showSideMenuHandler }: Props) => (
                             className='object-contain'
                         />
                     </div>
-                    <p className='font-semibold text-lg mt-1.5'>SHAWARMA SULTAN</p>
+                    <p className='font-semibold text-lg'>SHAWARMA SULTAN</p>
                 </div>
 
                 <ul className='flex items-center justify-center gap-20 font-semibold text-[#343434] text-lg'>
@@ -64,7 +64,7 @@ const Header = ({ showSideMenuHandler }: Props) => (
                     </li>
                 </ul>
 
-                <div className='invisible md:visible inline-flex justify-end items-center gap-3 text-[#343434]'>
+                <div className='invisible lg:visible inline-flex justify-end items-center gap-3 text-[#343434]'>
                     <span className="material-symbols-rounded text-4xl">person</span>
                     <span className="material-symbols-rounded text-4xl">shopping_basket</span>
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="md:container md:mx-auto flex flex-col items-center justify-between overflow-hidden">
-      <section className="hero-section w-full	h-[calc(100vh-95px)] p-4 block md:flex">
-        <div className="w-full md:w-6/12 flex flex-col items-start justify-center gap-6">
+      <section className="hero-section w-full	h-[calc(100vh-95px)] p-4 flex flex-col gap-4 lg:flex-row lg:gap-0">
+        <div className="w-full lg:w-6/12 flex flex-col items-start justify-center gap-6 h-1/2 lg:h-full">
           <h1 className='text-4xl font-semibold'>The Mobile Kabab Experience: <span className='underline decoration-[#f04637]'>Wherever</span>, <span className='underline decoration-[#f04637]'>Whenever</span></h1>
           <p className='text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
           <button className='bg-[#f04637] hover:bg-[#e64335] py-3 px-10 text-white'>See More</button>
         </div>
-        <div className="w-full md:w-6/12 relative">
-          {/* <div className='absolute bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2 w-9/12 h-3/4 bg-[#f04637]'></div> */}
-          <div className='absolute bottom-1/2 right-1/2 translate-y-1/2 translate-x-1/2 w-10/12 h-5/6'>
+        <div className="w-full lg:w-6/12 relative h-1/2 lg:h-full mx-1 lg:mx-0">
+          <div className='absolute lg:bottom-[47.5%] lg:right-[47.5%] lg:translate-y-1/2 lg:translate-x-1/2 w-full lg:w-10/12 h-full lg:h-5/6 bg-[#f04637]'></div>
+          <div className='absolute bottom-3 lg:bottom-1/2 right-3 lg:right-1/2 lg:translate-y-1/2 lg:translate-x-1/2 w-full lg:w-10/12 h-full lg:h-5/6'>
             <Image
               src='/assets/images/hero-section-photo.avif'
               alt='Shawarma Sultan Van'
